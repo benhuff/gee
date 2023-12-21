0398d77b951bf5e3b156e0a1d5c22edc860e69da
benhuff <52004338+benhuff@users.noreply.github.com>
2023-12-21 00:32:59 -0500

    Update precip.js

--- precip.js
+++ precip.js
@@ -1,83 +1,194 @@
-var chirpsPentad = ee.ImageCollection("UCSB-CHG/CHIRPS/PENTAD");
-print('Pentad',chirpsPentad.filterDate('2018-10-01','2018-10-31'));
+var table = table 
 
-var chirpsCrs = 'EPSG:4326';
-var chirpsCrsTransform = [0.05,0,-180,0,-0.05,50];
+Map.centerObject(table, 5);
+Map.addLayer(table, {}, 'Lower Mekong basin');
 
-var months = ee.List.sequence(1,12);
-var nullImage = ee.Image(0);
+// Set start and end years.
+var startYear = 1987;
+var endYear = 2015;
 
-var map_m = function(i) {
-  i = ee.Number(i);
-  var years = ee.List.sequence(1987, 2016);
-  var filtered_col = years.map(function(j) {
-    var filtered = chirpsPentad.filter(ee.Filter.calendarRange(i, i, 'month'))
-                               .filter(ee.Filter.calendarRange(j, j, 'year'));
-    var time = filtered.first().get('system:time_start');
-    return ee.Algorithms.If(filtered.size().gt(5),
-                            filtered.sum().set('year',j).set('month',i.format('%02d')).set('system:time_start',time).set('constant','n'),
-                            nullImage.set('year',j).set('month',i).set('system:time_start',ee.Date.fromYMD(j,i,01).millis()).set('constant','y'));
-  });
-  return filtered_col;
+// Create two date objects for start and end years.
+var startDate = ee.Date.fromYMD(startYear, 1, 1);
+var endDate = ee.Date.fromYMD(endYear + 1, 1, 1);
+
+// Make a list with years.
+var years = ee.List.sequence(startYear, endYear);
+
+// Make a list with months.
+var months = ee.List.sequence(1, 12);
+
+// Import the CHIRPS dataset.
+var CHIRPS = ee.ImageCollection('UCSB-CHG/CHIRPS/PENTAD');
+
+// Filter for the relevant time period.
+CHIRPS = CHIRPS.filterDate(startDate, endDate);
+
+var scale = CHIRPS.select('precipitation').first().projection().nominalScale().getInfo();
+
+var climatology = ee.ImageCollection.fromImages(
+    years.map(function(y) {
+        return months.map(function(m) {
+            var w = CHIRPS.filter(ee.Filter
+                    .calendarRange(y, y, 'year'))
+                .filter(ee.Filter.calendarRange(m, m,
+                    'month'))
+                .sum();
+            return w.set('year', y)
+                .set('month', m)
+                .set('system:time_start', ee.Date
+                    .fromYMD(y, m, 1));
+
+        });
+    }).flatten()
+);
+
+var monthlyMean = ee.ImageCollection.fromImages(
+    months.map(function(m) {
+      var w = climatology.filter(ee.Filter.eq('month', m))
+          .mean();
+      return w.set('month', m);
+    })  
+);
+
+print(monthlyMean);
+
+var chartMeans = ui.Chart.image.seriesByRegion({
+        imageCollection: monthlyMean,
+        regions: table,
+        reducer: ee.Reducer.mean(),
+        band: 'precipitation',
+        scale: scale,
+        xProperty: 'month',
+        seriesProperty: 'ADM1_EN'
+    })
+    .setOptions(title)
+    .setChartType('LineChart');
+
+// Print the chart.
+print(chartMeans);
+
+// Set start and end years.
+var startYear = 2015;
+var endYear = 2020;
+
+// Create two date objects for start and end years.
+var startDate = ee.Date.fromYMD(startYear, 1, 1);
+var endDate = ee.Date.fromYMD(endYear + 1, 1, 1);
+
+// Make a list with years.
+var years = ee.List.sequence(startYear, endYear);
+
+// Make a list with months.
+var months = ee.List.sequence(1, 12);
+
+// Import the CHIRPS dataset.
+var CHIRPS = ee.ImageCollection('UCSB-CHG/CHIRPS/PENTAD');
+
+// Filter for the relevant time period.
+CHIRPS = CHIRPS.filterDate(startDate, endDate);
+
+var scale = CHIRPS.select('precipitation').first().projection().nominalScale().getInfo();
+
+// We apply a nested loop where we first map over 
+// the relevant years and then map over the relevant 
+// months. The function returns an image with the total (sum)
+// rainfall for each month. A flatten is applied to convert a
+// feature collection of features into a single feature collection.
+var monthlyPrecip = ee.ImageCollection.fromImages(
+    years.map(function(y) {
+        return months.map(function(m) {
+            var clim = climatology.filter(ee.Filter.eq('month', m)).mean().rename('climatology');
+            var w = CHIRPS.filter(ee.Filter
+                    .calendarRange(y, y, 'year'))
+                .filter(ee.Filter.calendarRange(m, m,
+                    'month'))
+                .sum();
+            var anom = w.subtract(clim).rename('anomaly');
+            var w2 = w.addBands([clim, anom]);
+            return w2.set('year', y)
+                .set('month', m)
+                .set('system:time_start', ee.Date
+                    .fromYMD(y, m, 1));
+
+        });
+    }).flatten()
+);
+
+// Set the title and axis labels for the chart.
+var title = {
+    title: 'Monthly precipitation',
+    hAxis: {
+        title: 'Time'
+    },
+    vAxis: {
+        title: 'Precipitation (mm)'
+    },
 };
 
-var imgList_months = months.map(map_m);
+// Plot the chart using the boundary.
+var chartMonthly = ui.Chart.image.seriesByRegion({
+        imageCollection: monthlyPrecip,
+        regions: table,
+        reducer: ee.Reducer.mean(),
+        band: 'precipitation',
+        scale: scale,
+        xProperty: 'system:time_start',
+        seriesProperty: 'ADM1_EN'
+    })
+    .setOptions(title)
+    .setChartType('LineChart');
 
-var imgCol = ee.ImageCollection.fromImages(imgList_months.flatten()).sort('system:time_start').filterMetadata('constant','equals','n');
-print('Sorted Image Collection', imgCol);
+// Print the chart.
+print(chartMonthly);
 
-var imgCol_forMean = imgCol.filterDate('1987-01-01','2015-01-01');
-var imgList_forMean = imgCol_forMean.toList(100);
-print('Image List for Mean', imgList_forMean);
+// Set the title and axis labels for the chart.
+var title = {
+    title: 'Monthly precipitation climatology',
+    hAxis: {
+        title: 'Time'
+    },
+    vAxis: {
+        title: 'Precipitation (mm)'
+    },
+};
+// Plot the chart using the boundary.
+var chartMonthly = ui.Chart.image.seriesByRegion({
+        imageCollection: monthlyPrecip,
+        regions: table,
+        reducer: ee.Reducer.mean(),
+        band: 'climatology',
+        scale: scale,
+        xProperty: 'system:time_start',
+        seriesProperty: 'ADM1_EN'
+    })
+    .setOptions(title)
+    .setChartType('LineChart');
 
-var index = [1,2,3,4,5,6,7,8,9,10,11,12];
-var meanList = ee.List([]);
+// Print the chart.
+print(chartMonthly);
 
-for (var i in index){
-  var m = index[i];
-  var imgColm = imgCol_forMean.filter(ee.Filter.calendarRange(m,m,'month'));
-  var meanImg = imgColm.mean().rename('mean').set('month',ee.Number(m).format('%02d'));
-  var meanList = meanList.add(meanImg);
-}
+// Set the title and axis labels for the chart.
+var title = {
+    title: 'Monthly precipitation anomaly',
+    hAxis: {
+        title: 'Time'
+    },
+    vAxis: {
+        title: 'Precipitation (mm)'
+    },
+};
+// Plot the chart using the boundary.
+var chartMonthly = ui.Chart.image.seriesByRegion({
+        imageCollection: monthlyPrecip,
+        regions: table,
+        reducer: ee.Reducer.mean(),
+        band: 'anomaly',
+        scale: scale,
+        xProperty: 'system:time_start',
+        seriesProperty: 'ADM1_EN'
+    })
+    .setOptions(title)
+    .setChartType('LineChart');
 
-var meanCol = ee.ImageCollection.fromImages(meanList);
-print('Mean Collection', meanCol);
-
-var Anom = imgCol.filterMetadata('constant','equals','n').map(function(image){
-  var month = ee.Number(image.get('month'));
-  var monthMean = meanCol.filterMetadata('month','equals',month).first().rename('mean');
-  var anomaly = image.subtract(monthMean).rename('anomaly');
-  return image.addBands([anomaly,monthMean]);
-});
-
-print('imgList w/ Anomaly + Mean',Anom);
-//Map.addLayer(Anom.first().clip(aoi),{bands:['anomaly'],min:-10,max:10,palette:['red','white','blue']});
-var chart = ui.Chart.image.seriesByRegion({
-  imageCollection: Anom,
-  regions:aoi,
-  band: 'precipitation',
-  reducer:ee.Reducer.mean(),
-  scale:5565,
-  xProperty:'system:time_start',
-  seriesProperty:'ADM1_EN'}).setChartType('LineChart');
-print(chart);
-
-var chart2 = ui.Chart.image.seriesByRegion({
-  imageCollection: Anom,
-  regions:aoi,
-  band: 'mean',
-  reducer:ee.Reducer.mean(),
-  scale:5565,
-  xProperty:'system:time_start',
-  seriesProperty:'ADM1_EN'}).setChartType('LineChart');
-print(chart2);
-
-var chart3 = ui.Chart.image.seriesByRegion({
-  imageCollection: Anom,
-  regions:aoi,
-  band: 'anomaly',
-  reducer:ee.Reducer.mean(),
-  scale:5565,
-  xProperty:'system:time_start',
-  seriesProperty:'ADM1_EN'}).setChartType('LineChart');
-print(chart3);
+// Print the chart.
+print(chartMonthly);
